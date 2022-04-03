1ced0499783bed82deab994011f07386fc45cc53
Taylor Bromley <taylorbromley@sky.com>
2022-04-03 15:29:00 +0100

    Refill grid and tile fall

--- Assets/Scripts/BoardGrid.cs
+++ Assets/Scripts/BoardGrid.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public class BoardGrid : MonoBehaviour
 {
     public int gridX;
     public int gridY;
+    public int offset;
+
     public GameObject tilePrefab;
     public GameObject[] tiles;
     public GameObject[,] allTiles;
@@ -29,13 +31,18 @@ public class Grid : MonoBehaviour
         {
             for (int tileY = 0; tileY < gridY; tileY++)
             {
-                Vector2 tempPos = new Vector2(tileX, tileY);
+                Vector2 tempPos = new Vector2(tileX, tileY + offset);
+
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPos, Quaternion.identity) as GameObject;
                 backgroundTile.transform.parent = this.transform;
                 backgroundTile.name = "tilesArr[" + tileX + "][" + tileY + "]";
 
                 int tileToUse = Random.Range(0, tiles.Length);
-                GameObject tile = Instantiate(tiles[tileToUse], tempPos, Quaternion.identity);
+                GameObject tile = Instantiate(tiles[tileToUse], tempPos, Quaternion.identity) as GameObject;
+
+                tile.GetComponent<Tile>().row = tileY;
+                tile.GetComponent<Tile>().column = tileX;
+
                 tile.transform.parent = this.transform;
                 tile.name = "tilesArr[" + tileX + "][" + tileY + "]";
                 allTiles[tileX, tileY] = tile;
@@ -48,7 +55,9 @@ public class Grid : MonoBehaviour
         foreach (GameObject tile in islands[index])
         {
             Destroy(tile);
+            allTiles[tile.GetComponent<Tile>().column, tile.GetComponent<Tile>().row] = null;
         }
+        StartCoroutine(TilesFall());
     }
 
     public void FindIslands(GameObject[,] grid)
@@ -87,5 +96,58 @@ public class Grid : MonoBehaviour
         DFS(grid, visited, i - 1, j, tag, islandIndex, island);
         DFS(grid, visited, i, j + 1, tag, islandIndex, island);
         DFS(grid, visited, i, j - 1, tag, islandIndex, island);
+    }
+
+    private IEnumerator TilesFall()
+    {
+        int emptyCount = 0;
+        for (int i = 0; i < gridX; i++)
+        {
+            for (int j = 0; j < gridY; j++)
+            {
+                if (allTiles[i,j] == null)
+                {
+                    emptyCount++;
+                }
+                else if (emptyCount > 0)
+                {
+                    allTiles[i, j].GetComponent<Tile>().row -= emptyCount;
+                    allTiles[i, j] = null;
+                }
+            }
+            emptyCount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
+        StartCoroutine(FillGrid());
+    }
+
+    private void RefillGrid()
+    {
+        for (int i = 0; i < gridX; i++)
+        {
+            for (int j = 0; j < gridY; j++)
+            {
+                if(allTiles[i,j] == null)
+                {
+                    Vector2 tempPos = new Vector2(i, j + offset);
+                    int tileToUse = Random.Range(0, tiles.Length);
+                    GameObject tile = Instantiate(tiles[tileToUse], tempPos, Quaternion.identity);
+                    allTiles[i, j] = tile;
+                    tile.GetComponent<Tile>().row = j;
+                    tile.GetComponent<Tile>().column = i;
+                }
+            }
+        }
+    }
+
+    private IEnumerator FillGrid()
+    {
+        RefillGrid();
+
+        islands.Clear();
+        FindIslands(allTiles);
+
+        yield return new WaitForSeconds(.5f);
+        FindObjectOfType<Controls>().SetPlayable(true);
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,13 +5,55 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     [SerializeField] public int islandListIndex;
+    public int row;
+    public int column;
+    public int targetRow;
+    public float fallSpeed = .5f;
+
+    private GameObject tileBelow;
+    private BoardGrid grid;
+    private Controls controls;
+    private Vector2 tempPos;
+
+    void Start()
+    {
+        grid = FindObjectOfType<BoardGrid>();
+        controls = FindObjectOfType<Controls>();
+    }
+
+    void Update()
+    {
+        targetRow = row;
+        
+        if (Mathf.Abs(targetRow - transform.position.y) > .1)
+        {
+            tempPos = new Vector2(transform.position.x, targetRow);
+            transform.position = Vector2.Lerp(transform.position, tempPos, fallSpeed);
+        }
+        else
+        {
+            tempPos = new Vector2(transform.position.x, targetRow);
+            transform.position = tempPos;
+            grid.allTiles[column, row] = this.gameObject;
+        }
+    }
 
     private void OnMouseDown()
     {
-        GameObject Grid = GameObject.Find("Grid");
-        if (Grid.GetComponent<Grid>().islands[islandListIndex].Count > 1)
+        if (controls.GetPlayable() && grid.islands[islandListIndex].Count > 1)
         {
-            Grid.GetComponent<Grid>().DestroyIsland(islandListIndex);
+            controls.SetPlayable(false);
+            grid.DestroyIsland(islandListIndex);
+        }
+    }
+
+    void MoveTile()
+    {
+        if (row > 0)
+        {
+            tileBelow = grid.allTiles[column, row - 1];
+            tileBelow.GetComponent<Tile>().row += 1;
+            row -= 1;
         }
     }
 }
